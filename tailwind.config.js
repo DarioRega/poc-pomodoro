fb7069ef6dc0489f55f9b2bb1512997d7eaf8de0
DarioRega <dario.regazzoni@outlook.fr>
2021-04-29 10:59:00 +0200

    New: tailwind config colors.

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,23 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        success: '#61B45B',
+        error: '#BF2C44',
+        celeste: '#D4DFF6',
+        'light-indigo': '#638FEF',
+        'dark-indigo': '#4f46e5',
+        'dark-blue': '#182532',
+        'darker-blue': '#131E28',
+        'dark-gray': '#747C84',
+        'darker-gray': '#333B44',
+        'lighter-white': '#F9F9F9',
+        'light-white': '#F4F5F7',
+        'dark-black': '#1C1D1F',
+        'darker-black': '#131415',
+      },
+    },
   },
   variants: {
     extend: {},
