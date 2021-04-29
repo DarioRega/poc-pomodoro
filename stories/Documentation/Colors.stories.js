db83a2795a1ec2d4a7c756a8d81a8454d209d049
DarioRega <dario.regazzoni@outlook.fr>
2021-04-29 14:02:20 +0200

    New: Create Typography component story, add sizes

---
+++ stories/Documentation/Colors.stories.js
@@ -0,0 +1,31 @@
+import ColorPalette from './Components/ColorPalette'
+
+export default {
+  title: 'Documentation/Colors',
+  component: ColorPalette,
+}
+
+export const All = () => ({
+  data() {
+    return {
+      colors: [
+        'bg-success',
+        'bg-error',
+        'bg-celeste',
+        'bg-light-indigo',
+        'bg-dark-indigo',
+        'bg-dark-blue',
+        'bg-darker-blue',
+        'bg-dark-gray',
+        'bg-darker-gray',
+        'bg-lighter-white',
+        'bg-light-white',
+        'bg-dark-black',
+        'bg-darker-black',
+      ],
+    }
+  },
+  components: { ColorPalette },
+  template: `
+    <color-palette :colors="colors" />`,
+})
---
+++ stories/Documentation/Components/Typography.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>Lorem Ipsum - h1</h1>
+    <h2>Lorem Ipsum - h2</h2>
+    <h3>Lorem Ipsum - h3</h3>
+    <h4>Lorem Ipsum - h4</h4>
+    <h5>Lorem Ipsum - h5</h5>
+    <h6>Lorem Ipsum - h6</h6>
+    <p>Lorem Ipsum - paragraph</p>
+    <a>Lorem Ipsum - link</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Typography',
+}
+</script>
