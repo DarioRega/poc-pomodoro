01b860badfb3a486bc949de5b36d9c18d18abf4b
dario <dario.regazzoni@outlook.fr>
2021-04-30 09:40:42 +0200

    Fix: Re-arrange button secondary / disabled (dark theme) styling. Remove tailwind color light celeste and keep only celeste

--- tailwind.config.js
+++ tailwind.config.js
@@ -25,8 +25,7 @@ module.exports = {
       colors: {
         success: '#61B45B',
         error: '#BF2C44',
-        'light-celeste': '#edf2fb',
-        celeste: '#D4DFF6',
+        celeste: '#EDF2FB',
         'light-indigo': '#638FEF',
         'dark-indigo': '#4f46e5',
         'dark-blue': '#182532',
