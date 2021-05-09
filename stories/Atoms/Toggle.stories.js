c50f7eab73f55501a010bd4866bb57057cd9aedf
Dario Regazzoni <dario.regazzoni@outlook.fr>
2021-05-09 12:41:32 +0000

    New: Toggle component

---
+++ stories/Atoms/Toggle.stories.js
@@ -0,0 +1,47 @@
+import Toggle from '../../components/Atoms/Toggle'
+
+export default {
+  title: 'Atoms/Toggle',
+  component: Toggle,
+  argTypes: {
+    toggled: {
+      control: { type: 'radio', options: [true, false] },
+    },
+    withIcon: {
+      control: { type: 'radio', options: [false, true] },
+    },
+  },
+}
+
+const Template = (args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  components: { Toggle },
+  methods: {
+    toggle(value) {
+      this.toggled = value
+    },
+  },
+  template: `<toggle v-bind="$props" @toggle="toggle" />`,
+})
+
+export const UnToggled = Template.bind({})
+UnToggled.args = {
+  toggled: false,
+}
+
+export const Toggled = Template.bind({})
+Toggled.args = {
+  toggled: true,
+}
+
+export const UnToggledWithIcon = Template.bind({})
+UnToggledWithIcon.args = {
+  toggled: false,
+  withIcon: true,
+}
+
+export const ToggledWithIcon = Template.bind({})
+ToggledWithIcon.args = {
+  toggled: true,
+  withIcon: true,
+}
