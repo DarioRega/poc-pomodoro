2e8184ead345069453ec26c3b64ffa0c4034a3c2
Renato Moor <renatomoor1@gmail.com>
2021-05-09 14:24:56 +0200

    Docs: Add pomodoro temporary logo

---
+++ config/theme.js
@@ -0,0 +1,10 @@
+import { create } from '@storybook/theming'
+
+export default create({
+  colorPrimary: '#4F46E5',
+  colorSecondary: '#638FEF',
+  base: 'light',
+  brandTitle: 'Pomodoro Styleguide',
+  brandUrl: 'https://styleguide.pomodoro.seelo.ch',
+  brandImage: 'https://doc.pomodoro.seelo.ch/logo-light.svg',
+})
