5378fec538ea4059261d0a5ea86671947996b38e
dario <dario.regazzoni@outlook.fr>
2021-06-05 19:51:16 +0200

    Fix: Regroup actions on timer / session in actions.js, dispatch directly action with mapActions on TimerSidebar and TimerScreenExpander, to have only one source of truth

--- components/Organisms/TimerScreenExpander.vue
+++ components/Organisms/TimerScreenExpander.vue
@@ -3,12 +3,12 @@
     <timer-screen-expander-clock />
     <div class="mt-16">
       <timer-screen-expander-controls
-        @onStart="handleStartTimer"
-        @onPause="handlePauseTimer"
-        @onResume="handleResumeTimer"
-        @onSkip="handleSkipProcess"
-        @onAbort="handleAbort"
-        @onStartSession="handleStartSession"
+        @onStart="startCurrentStep"
+        @onPause="pauseCurrentStep"
+        @onResume="resumeCurrentStep"
+        @onSkip="onSkipCurrentStepClick"
+        @onAbort="onAbortClick"
+        @onStartSession="startSession"
       />
     </div>
   </screen-expander>
@@ -35,46 +35,13 @@ export default {
   },
   methods: {
     ...mapActions({
-      createNotification: 'globalState/createNotification',
+      onAbortClick: 'sessions/onAbortClick',
+      onSkipCurrentStepClick: 'sessions/onSkipCurrentStepClick',
+      pauseCurrentStep: 'sessions/pauseCurrentStep',
+      resumeCurrentStep: 'sessions/resumeCurrentStep',
+      startCurrentStep: 'sessions/startCurrentStep',
+      startSession: 'sessions/startSession',
     }),
-    handleStartSession() {
-      // TODO handle cases
-    },
-    handleStartTimer() {
-      // TODO handle
-    },
-    handlePauseTimer() {
-      // TODO handle
-    },
-    handleResumeTimer() {
-      // TODO handle
-    },
-    handleAbort() {
-      const notification = {
-        title: 'Abort session ?',
-        description: 'Are you sure to abort the current session ?',
-        actionRequired: true,
-        confirmCallback: this.abortCurrentSession,
-      }
-      this.createNotification(notification)
-    },
-
-    handleSkipProcess() {
-      // TODO handle cases
-      const notification = {
-        title: 'Skip process ?',
-        description: 'Are you sure to skip the current process ?',
-        actionRequired: true,
-        confirmCallback: this.skipCurrentProcess(),
-      }
-      this.createNotification(notification)
-    },
-    abortCurrentSession() {
-      // TODO dispatch action
-    },
-    skipCurrentProcess() {
-      // TODO dispatch action
-    },
   },
 }
 </script>
