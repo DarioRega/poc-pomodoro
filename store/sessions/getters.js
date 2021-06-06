91c8629623e54b5a75ecdd56735eb1041b99c767
dario <dario.regazzoni@outlook.fr>
2021-06-06 15:44:38 +0200

    Fix: Refactor and add todos for getter in session

--- store/sessions/getters.js
+++ store/sessions/getters.js
@@ -1,29 +1,38 @@
 import moment from 'moment-timezone'
-import { POMODORO_STATUS, STEPS_STATUS } from '@/constantes'
+import { STEPS_STATUS } from '@/constantes'
+import { calculateSessionEndTime } from '@/helpers/sessions'
 
 export default {
   getTimerState: (state) => {
     return {
-      isRunning: state.currentStatus.includes(STEPS_STATUS.IN_PROGRESS),
-      isPaused: state.currentStatus.includes(STEPS_STATUS.PAUSED),
-      isPending: state.currentStatus.includes(STEPS_STATUS.PENDING),
-      isSessionPending: state.currentStatus.includes(
-        POMODORO_STATUS.SESSION.PENDING
-      ),
+      isRunning: state.currentStep.status.includes(STEPS_STATUS.IN_PROGRESS),
+      isPaused: state.currentStep.status.includes(STEPS_STATUS.PAUSED),
+      isPending: state.currentStep.status.includes(STEPS_STATUS.PENDING),
+      isSessionPending: state.session.status.includes(STEPS_STATUS.PENDING),
       isSessionStartedButPendingProcess:
-        state.currentStatus.includes(STEPS_STATUS.PENDING) &&
-        !state.currentStatus.includes(POMODORO_STATUS.SESSION.PENDING),
+        state.currentStep.status.includes(STEPS_STATUS.PENDING) &&
+        !state.session.status.includes(STEPS_STATUS.PENDING),
     }
   },
-  getCurrentTimer: (state) => {
-    // TODO check here if paused, give resting time, if not paused give timer
-    return state.currentTimer
-  },
   getCurrentSessionEndTime: (state) => {
-    // TODO check here if paused, give resting time + date currentSessionEndtime (addition), or else format date moment
-    return moment(state.currentSessionEndTime).format('hh:mm A')
+    if (state.session.status.includes(STEPS_STATUS.PAUSED)) {
+      // TODO renato give us resting time from the session directly or i need to calculate from steps ?
+      // if i need to calculate set the state.session_end_time and in progress as first condition,
+      // else give what i wrote already on the else
+    } else if (state.session.end_time) {
+      return moment(state.session).format('hh:mm A')
+    } else {
+      const calculatedEndTime = calculateSessionEndTime(state.session.steps)
+      // TODO format depending user settings
+      return moment(calculatedEndTime).format('hh:mm A')
+    }
   },
   getSessionSteps: (state) => {
-    return state.sessionSteps
+    return state.session.steps
   },
+  isSessionStarted: (state) =>
+    state.session.status === STEPS_STATUS.IN_PROGRESS ||
+    STEPS_STATUS.IN_PROGRESS,
+
+  isSessionPaused: (state) => state.session.status === STEPS_STATUS.PAUSED,
 }
