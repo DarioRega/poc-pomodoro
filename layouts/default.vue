fbb73e4bba79a3cbb02efef2d682c1ca1d15ecae
dario <dario.regazzoni@outlook.fr>
2021-06-07 21:37:27 +0200

    New: Add screen loader to default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,12 +3,22 @@
     <Nuxt />
     <!--  Notifications -->
     <notifications-container />
+    <screen-loader v-if="isEnvLoading">
+      <h5 class="font-body tracking-wider text-dark-blue dark:text-celeste">
+        {{ $t('Loading your environment') }}
+      </h5>
+    </screen-loader>
   </div>
 </template>
 <script>
 import NotificationsContainer from '@/components/Templates/NotificationsContainer'
-
+import ScreenLoader from '@/components/Atoms/Loaders/ScreenLoader'
 export default {
-  components: { NotificationsContainer },
+  components: { NotificationsContainer, ScreenLoader },
+  computed: {
+    isEnvLoading() {
+      return this.$store.state.globalState.isEnvLoading
+    },
+  },
 }
 </script>
