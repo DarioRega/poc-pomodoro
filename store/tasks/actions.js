e63a34794ec1928628711e29b0ff054495275ef6
dario <dario.regazzoni@outlook.fr>
2021-06-12 17:45:14 +0200

    Fix: Remove return when addTask call is successful, on return error

--- store/tasks/actions.js
+++ store/tasks/actions.js
@@ -3,8 +3,7 @@ import { TASK_URL } from '@/constantes/api'
 export default {
   async addTask({ dispatch, commit }, payload) {
     try {
-      const { data } = await this.$axios.post(TASK_URL, payload)
-      return data
+      await this.$axios.post(TASK_URL, payload)
     } catch (err) {
       return err.response.data
     }
