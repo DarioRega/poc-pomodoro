da1ee209238bb88020bec541934c72aca23ad755
dario <dario.regazzoni@outlook.fr>
2021-06-13 19:55:56 +0200

    Fix: Remove all notifications creation on tasks WS events handlers

--- EchoEventsHandlers/tasks.js
+++ EchoEventsHandlers/tasks.js
@@ -1,29 +1,16 @@
-export const onCreateTaskEvent = (payload, store, i18n) => {
+export const onCreateTaskEvent = (payload, store) => {
   if (payload) {
-    const notification = {
-      title: i18n.t('Task created!'),
-      type: 'success',
-      description: i18n.t('A new task was added to your list'),
-    }
-    store.dispatch('globalState/createNotification', notification)
-
     store.commit('tasks/ADD_SINGLE_TASK', payload)
   }
 }
 
-export const onUpdateTaskEvent = (payload, store, i18n) => {
+export const onUpdateTaskEvent = (payload, store) => {
   if (payload) {
-    const notification = {
-      title: i18n.t('Task edited!'),
-      type: 'success',
-      description: i18n.t('Your task was successfully edited'),
-    }
-    store.dispatch('globalState/createNotification', notification)
     store.dispatch('tasks/updateSingleTask', payload)
   }
 }
 
-export const onDeleteTaskEvent = (payload, store, i18n) => {
+export const onDeleteTaskEvent = (payload, store) => {
   if (payload) {
     store.dispatch('tasks/deleteSingleTask', payload)
   }
