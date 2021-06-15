6f0d59dda6d841455523c2fcb09011a5876e64b3
dario <dario.regazzoni@outlook.fr>
2021-06-15 13:06:32 +0200

    Fix: Comment unused fields for MVP in SettingsPanelGeneralTab.vue

--- components/Organisms/SettingsPanels/SettingsPanelGeneralTab.vue
+++ components/Organisms/SettingsPanels/SettingsPanelGeneralTab.vue
@@ -55,57 +55,88 @@
       </div>
     </div>
 
-    <!--  BUG REPORTS  -->
-    <div class="settings-panel__container">
-      <div class="settings-panel__labels">
-        <h6>{{ $t('Bug reports') }}</h6>
-        <p>
-          {{
-            $t(
-              'Automatically send anonymous reports to help us improve our products'
-            )
-          }}
-        </p>
-      </div>
-      <div class="settings-panel__configurations">
-        <toggle
-          :toggled="values.bugReports"
-          class="justify-end"
-          @toggle="$emit('onBugReportsChange', $event)"
-        />
-      </div>
-    </div>
+    <!--    LANGUAGE v2 -->
+    <!--    <div class="settings-panel__container">-->
+    <!--      <div class="settings-panel__labels">-->
+    <!--        <h6>{{ $t('Display language') }}</h6>-->
+    <!--        <p>{{ $t('Set the language displayed in the application') }}</p>-->
+    <!--      </div>-->
+    <!--      <div class="settings-panel__configurations">-->
+    <!--        <brand-select-->
+    <!--          :value="values.displayLanguage"-->
+    <!--          :options="options.displayLanguages"-->
+    <!--          name="display-language"-->
+    <!--          @change="$emit('onDisplayLanguageChange', $event)"-->
+    <!--        />-->
+    <!--      </div>-->
+    <!--    </div>-->
 
-    <!--  ANALYTICS  -->
-    <div class="settings-panel__container">
-      <div class="settings-panel__labels">
-        <h6>{{ $t('Analytics') }}</h6>
-        <p>
-          {{
-            $t(
-              'Enable anonymous analytics to help us improve user experience. This includes clicks, tabs visits, view mode selected, language used and sessions settings'
-            )
-          }}
-        </p>
-      </div>
-      <div class="settings-panel__configurations">
-        <toggle
-          :toggled="values.analytics"
-          class="justify-end"
-          @toggle="$emit('onAnalyticsChange', $event)"
-        />
-      </div>
-    </div>
+    <!--  TIMEZONE  v2 -->
+    <!--    <div class="settings-panel__container">-->
+    <!--      <div class="settings-panel__labels">-->
+    <!--        <h6>{{ $t('Time zone') }}</h6>-->
+    <!--        <p>{{ $t('Select your current time zone') }}</p>-->
+    <!--      </div>-->
+    <!--      <div class="settings-panel__configurations">-->
+    <!--        <brand-select-->
+    <!--          :value="values.timezone"-->
+    <!--          :options="options.timezones"-->
+    <!--          name="timezone"-->
+    <!--          @change="$emit('onTimezoneChange', $event)"-->
+    <!--        />-->
+    <!--      </div>-->
+    <!--    </div>-->
+
+    <!--  BUG REPORTS v2  -->
+    <!--    <div class="settings-panel__container">-->
+    <!--      <div class="settings-panel__labels">-->
+    <!--        <h6>{{ $t('Bug reports') }}</h6>-->
+    <!--        <p>-->
+    <!--          {{-->
+    <!--            $t(-->
+    <!--              'Automatically send anonymous reports to help us improve our products'-->
+    <!--            )-->
+    <!--          }}-->
+    <!--        </p>-->
+    <!--      </div>-->
+    <!--      <div class="settings-panel__configurations">-->
+    <!--        <toggle-->
+    <!--          :toggled="values.bugReports"-->
+    <!--          class="justify-end"-->
+    <!--          @toggle="$emit('onBugReportsChange', $event)"-->
+    <!--        />-->
+    <!--      </div>-->
+    <!--    </div>-->
+
+    <!--  ANALYTICS v2  -->
+    <!--    <div class="settings-panel__container">-->
+    <!--      <div class="settings-panel__labels">-->
+    <!--        <h6>{{ $t('Analytics') }}</h6>-->
+    <!--        <p>-->
+    <!--          {{-->
+    <!--            $t(-->
+    <!--              'Enable anonymous analytics to help us improve user experience. This includes clicks, tabs visits, view mode selected, language used and sessions settings'-->
+    <!--            )-->
+    <!--          }}-->
+    <!--        </p>-->
+    <!--      </div>-->
+    <!--      <div class="settings-panel__configurations">-->
+    <!--        <toggle-->
+    <!--          :toggled="values.analytics"-->
+    <!--          class="justify-end"-->
+    <!--          @toggle="$emit('onAnalyticsChange', $event)"-->
+    <!--        />-->
+    <!--      </div>-->
+    <!--    </div>-->
   </section>
 </template>
 
 <script>
 import BrandSelect from '@/components/Atoms/Inputs/BrandSelect'
-import Toggle from '@/components/Atoms/Inputs/Toggle'
 
 export default {
   name: 'SettingsPanelGeneralTab',
-  components: { BrandSelect, Toggle },
+  components: { BrandSelect },
   props: {
     values: {
       type: Object,
