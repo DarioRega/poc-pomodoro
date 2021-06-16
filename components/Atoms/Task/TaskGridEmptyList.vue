e672c5beb77e66d9b0a6193f0b4458e49037fd3d
dario <dario.regazzoni@outlook.fr>
2021-06-16 23:29:58 +0200

    Fix: Change text size message when list empty, remove not funny joke as well on it

--- components/Atoms/Task/TaskGridEmptyList.vue
+++ components/Atoms/Task/TaskGridEmptyList.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="flex justify-center items-center h-full">
-    <h5 class="font-body text-dark-gray">
-      {{ label ? label : $t('Your list is currently empty... take a day off') }}
-    </h5>
+    <h6 class="font-body text-dark-gray">
+      {{ label ? label : $t('Your list is currently empty.') }}
+    </h6>
   </div>
 </template>
 
