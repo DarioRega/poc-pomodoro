0da6ced1bdad0cf99033ca0737fa5703000b35c3
dario <dario.regazzoni@outlook.fr>
2021-06-17 22:28:55 +0200

    Fix: Remove commented code

--- store/timers/getters.js
+++ store/timers/getters.js
@@ -1,25 +1,9 @@
-// import { STEPS_STATUS } from '@/constantes'
-// import { transformHoursDurationFormatToMinutesDurationFormat } from '@/helpers/sessions'
-
 export default {
-  getCurrentStepTimer: (state, getters, rootState, rootGetters) => {
-    // if (rootGetters['sessions/hasCurrentSession']) {
-    //   const {
-    //     sessions: {
-    //       current: {
-    //         current_step: { status, resting_time },
-    //       },
-    //     },
-    //   } = rootState
-    //   if (status.includes(STEPS_STATUS.PAUSED)) {
-    //     return transformHoursDurationFormatToMinutesDurationFormat(resting_time)
-    //   }
-    // }
-    // return state.currentStepTimer
+  getCurrentStepTimer: (state) => {
     return state.currentStepTimer
   },
 
-  getCurrentSessionEndTime: (state, getters, rootState, rootGetters) => {
+  getCurrentSessionEndTime: (state) => {
     return state.currentSessionEndTime
   },
 }
