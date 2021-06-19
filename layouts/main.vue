2ba593bd187423bd24b0730bafe0592be78cb45c
dario <dario.regazzoni@outlook.fr>
2021-06-19 12:25:56 +0200

    Fix: Hide main layout on < 1024 px, display a message to inform user

--- layouts/main.vue
+++ layouts/main.vue
@@ -1,23 +1,39 @@
 <template>
   <div :class="userTheme">
-    <Nuxt v-if="!isEnvLoading && !isRefreshLoading" />
-    <!--  Notifications -->
-    <notifications-container />
-    <screen-loader v-if="isEnvLoading || isRefreshLoading">
-      <h5 class="font-body tracking-wider text-dark-blue dark:text-celeste">
-        {{
-          isEnvLoading
-            ? $t('Loading your environment...')
-            : $t('Synchronizing channels...')
-        }}
-      </h5>
-    </screen-loader>
-    <transition-translate-y
-      duration-amount="500"
-      translate-y="-translate-y-full"
-    >
-      <launch-session-timer v-if="isLaunchTimerVisible" />
-    </transition-translate-y>
+    <div class="hidden lg:block">
+      <Nuxt v-if="!isEnvLoading && !isRefreshLoading" />
+      <!--  Notifications -->
+      <notifications-container />
+      <screen-loader v-if="isEnvLoading || isRefreshLoading">
+        <h5 class="font-body tracking-wider text-dark-blue dark:text-celeste">
+          {{
+            isEnvLoading
+              ? $t('Loading your environment...')
+              : $t('Synchronizing channels...')
+          }}
+        </h5>
+      </screen-loader>
+      <transition-translate-y
+        duration-amount="500"
+        translate-y="-translate-y-full"
+      >
+        <launch-session-timer v-if="isLaunchTimerVisible" />
+      </transition-translate-y>
+    </div>
+    <div class="lg:hidden h-screen flex items-center justify-center">
+      <div class="text-center">
+        <h4 class="text-dark-gray">
+          {{
+            $t(
+              'This layout is not available on device smaller than 1024 pixels wide'
+            )
+          }}
+        </h4>
+        <h6 class="text-dark-gray mt-6">
+          {{ $t('Stacked layout will be available on v2') }}
+        </h6>
+      </div>
+    </div>
   </div>
 </template>
 <script>
